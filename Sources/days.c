a953f2d974fa579c1bf3c83745d56e0f4ba454e2
mountain-ha <mountain-ha@naver.com>
2023-12-09 20:25:56 +0900

    날짜 계산

--- Sources/days.c
+++ Sources/days.c
@@ -18,6 +18,33 @@ int isLeapYear(int year) {
     return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
 }
 
+// n일 후의 날짜를 계산하는 함수
+struct Date calculateDate(struct Date currentDate, int n) {
+    // 윤년 여부 확인
+    int isLeap = isLeapYear(currentDate.year);
+
+    // 현재 날짜에 n일을 더함
+    currentDate.day += n;
+
+    //  현재 달의 일수를 초과한 만큼 날짜를 감소시키고, 이제 다음 달로 이동
+    while (currentDate.day > daysInMonth[isLeap][currentDate.month - 1]) {
+        currentDate.day -= daysInMonth[isLeap][currentDate.month - 1];
+        currentDate.month++;
+
+        if (currentDate.month > 12) {
+            currentDate.month = 1;
+            currentDate.year++;
+            isLeap = isLeapYear(currentDate.year);
+        }
     }
+
+    
+    return currentDate;
+}
+
+int main() {
+    printf("a");
+    return;
+}
 
    
